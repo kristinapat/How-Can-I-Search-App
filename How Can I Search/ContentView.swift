9823a9de2a69bc128b56aa64c4544c5749f20c6d
kristina paterson <kristina.s.paterson@gmail.com>
2025-07-02 17:10:17 -0500

    Initial Commit

---
+++ How Can I Search/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  How Can I Search
+//
+//  Created by Kristina Paterson on 7/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ How Can I Search/How_Can_I_SearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  How_Can_I_SearchApp.swift
+//  How Can I Search
+//
+//  Created by Kristina Paterson on 7/2/25.
+//
+
+import SwiftUI
+
+@main
+struct How_Can_I_SearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
